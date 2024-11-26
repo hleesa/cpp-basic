9333a52ae2afbcce5e208865fd1d7e59159f4fae
lsh <lshhi100@gmail.com>
2024-11-26 23:29:15 +0900

    member function

--- structToClass.cpp
+++ structToClass.cpp
@@ -5,26 +5,27 @@
 struct People { // type definition
     int age; // member variable
     char name[10];
+
+    void PrintPeople(People* this_){ //member function
+        printf("%s : %i\n", this_->name, this_->age);
+    }
+
+    void SetAge( People* this_, int newAge) {
+        this_->age = newAge;
+    }
 };
 
-void PrintPeople(People* a){
-    printf("%s : %i\n", a->name, a->age);
-}
-
-void SetAge( People* a, int newAge) {
-    a->age = newAge;
-}
 
 int main(){
     People* b;
 
-    b = (People*) malloc(sizeof(People));
+    b = new People;
     b->age = 21;
     strcpy(b->name, "World");
 
-    SetAge(b, b->age + 1);
-    PrintPeople(b);
-    free(b);
+    b->SetAge(b, b->age + 1);
+    b->PrintPeople(b);
+    delete b;
 
     return 0;
 }
