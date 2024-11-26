34ad8057c2dd35365468bcc9662ad5eeecaea7e2
lsh <lshhi100@gmail.com>
2024-11-26 22:51:43 +0900

    stack frame (visualization)

--- structToClass.cpp
+++ structToClass.cpp
@@ -6,23 +6,24 @@ struct People { // type definition
     char name[10];
 };
 
-People a;
-
 void PrintPeople(People a){
     printf("%s : %i\n", a.name, a.age);
 }
 
 int main(){
 
+    People a;
+    People b;
+
     a.age = 20;
     strcpy(a.name, "Hello");
+    char* cp = (char*) &a;
+    printf("%c\n", cp[5]);
 
-    People b;
     b.age = 21;
     strcpy(b.name, "World");
 
     PrintPeople(a);
-    PrintPeople(b);
 
     return 0;
 }
