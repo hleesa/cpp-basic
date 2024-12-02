a628dfe6f7aeb319fd23fdd53d00f54467af3813
lsh <lshhi100@gmail.com>
2024-12-02 20:20:50 +0900

    return reference

--- reference01.cpp
+++ reference01.cpp
@@ -1,30 +1,62 @@
 #include <cstdio>
 
-class KTest{
-public:
-    void Print() const { // declare this const
-        printf("Age = %i\n", age);
-    }
-    void IncAge(){
-        age += 1;
-    }
-private:
-    int age = 20;
-};
+#define REF
+#define IN
+#define OUT
 
-void Test(char* msg){
-//    msg[0] = 'S';
-    printf("%s\n", msg);
+//void Swap(int a, int b){ // call by value
+void Swap(int& a, int& b){ // call by l-value reference
+    int t = a;
+    a = b;
+    b = t;
 }
 
-int main(){
-    const float pi = 3.141592f;
-//    pi = 3.5;
-    Test("Hello");
+int& AccessInt(){
+//    int i = 1;
+    static int i = 1 ;
+    printf("%i\n", i);
+    return i;
+}
 
-    const KTest t;
-//    t.IncAge();
-    t.Print( );
+class KTest{
+public:
+//    KTest Print(int i){
+//    KTest& Print(int i){
+    KTest& operator() (int i){
+        printf("%i", i);
+        return *this;
+    }
+    KTest& operator() (float i){ // overloading
+        printf("%g", i);
+        return *this;
+    }
+    KTest& operator()  (const char* msg){ // overloading
+        printf(msg);
+        return *this;
+    }
+private:
+    int age = 0;
+};
+
+int main(){
+
+    int a = 2;
+    int b = 3;
+    &a;
+
+    Swap( IN OUT a, IN OUT b);
+    printf("%i %i\n", a, b);
+
+    AccessInt() = 3;
+    AccessInt();
+
+    KTest t;
+//    t.Print(a);
+//    t.Print(3.1f);
+//    t.Print("Hello");
+//    t.Print(a).Print(" ").Print(3.1f).Print(" ").Print("Hello");
+//    t.operator() (a).operator()(" ").operator()(3.1f).operator()(" ").operator()("Hello");
+    t(a)(" ")(3.1f)(" ")("Hello");
 
     return 0;
 }
